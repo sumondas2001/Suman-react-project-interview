039baa291b024ae8fd754c5f7a5561638fb27382
Sanjida Akter <sanjidatanha09@gmail.com>
2024-10-21 19:47:00 +0600

    kjkjj

--- src/Page/OrderDetails/OrderDetails.jsx
+++ src/Page/OrderDetails/OrderDetails.jsx
@@ -8,7 +8,7 @@ const OrderDetails = () => {
         
             <div className=" m-mt_16px">
                 <div className="w-full flex flex-col lg:flex-row items-start justify-center h-full gap-2 ">
-                    <div className="bg-white lg:p-p_30px w-full lg:w-[70%] ">
+                    <div className="bg-white lg:p-p_30px w-full  ">
                         <div className="text-center  flex flex-col justify-center items-center ">
                             <p className="text-xl font-bold">Order Information</p>
                             <p className="p-3 rounded-md lg:my-2 my-1 w-fit border bg-[#D2C5A2] font-bold text-lg">
@@ -20,7 +20,7 @@ const OrderDetails = () => {
                         </div>
                         <div className="w-full border flex flex-col md:flex-row md:items-start   md:mt-4 mt-3 bg-[#D2C5A2] rounded-md p-4  ">
                             <div className="md:text-base text-sm flex-1  font-semibold   md:border-r-2 md:border-black md:pr-10">
-                                <p className="font-bold md:mb-4 w-full">Shipping Address</p>
+                                <p className="font-bold md:mb-4 w-full">Demo information,Checkout page information will be here </p>
                                 <div className="space-y-1 w-full">
                                     <div className="flex items-center justify-between">
                                         <p>Full Name :</p>
@@ -56,65 +56,61 @@ const OrderDetails = () => {
                             </div>
 
                             <div className="md:text-base text-sm  flex-1 font-semibold  md:ml-10 mt-m_medium">
-                                <p className="font-bold  md:mb-4 w-full">Order Summary</p>
-                                <div className="space-y-1">
-                                    <div className="flex items-center justify-between">
-                                        <p>Subtotal Amount</p>
-                                        <p>
-                                           
-                                        </p>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <p>Shipping Charge</p>
-                                        <p>
-                                          
-                                        </p>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <p>Total Amount</p>
-                                        <p>
-                                           
-                                        </p>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <p>Due Amount</p>
-                                        <p></p>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <p>Paid Amount</p>
-                                        <p className="text-green-700">
-                                          
-                                        </p>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <p>Payment Type</p>
-                                        <p className="text-green-700">
-                                           
-                                        </p>
-                                    </div>
+                            <p className="font-bold  md:mb-4 w-full">Demo information,Checkout page information will be here </p>
+                            <div className="space-y-1 w-full">
+                                <div className="flex items-center justify-between">
+                                    <p>Full Name :</p>
+                                    <p className="text-start">
+                                        name
+                                    </p>
                                 </div>
+                                <div className="flex items-center justify-between">
+                                    <p>Country :</p>
+                                    <p>country</p>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <p>District Thana :</p>
+                                    <p className="text-start">
+                                        Thana
+                                    </p>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <p>Address :</p>
+                                    <p>Address</p>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <p>Order Notes :</p>
+                                    <p className="text-start">
+                                        Order Notes
+                                    </p>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <p>Mobile :</p>
+                                    <p>Mobile</p>
+                                </div>
+                            </div>
                             </div>
                         </div>
 
-                        <div className="lg:my-8 md:my-6 my-8">
+                        <div className="lg:my-8 md:my-6 my-8 px-p_4px">
                             <p className=" md:my-2 font-semibold">Courses:</p>
                             <table className="overflow-x-auto border w-full">
                                 <thead className="b w-full">
                                     <tr className="text-sm ">
                                         <th className="lg:w-20 md:w-16 w-8 py-2 md:py-4 lg:py-6 border ">
-                                            Unit Image
+                                            Image
                                         </th>
                                         <th className="lg:w-72 md:w-64 w-40 py-2 md:py-4 lg:py-6 border">
                                             Course Name
                                         </th>
                                         <th className="lg:w-72 md:w-64 w-40 py-2 md:py-4 lg:py-6 border">
-                                            Unit Name
+                                          Student   Name
                                         </th>
                                         <th className="lg:w-20 md:w-20 w-16 py-2 md:py-4 lg:py-6 border">
                                             Quantity
                                         </th>
                                         <th className="lg:w-20 md:w-20 w-16  py-2 md:py-4 lg:py-6 border text-center">
-                                            Unit Price
+                                         Price
                                         </th>
                                         <th className="lg:w-20 md:w-20 w-16  py-2 md:py-4 lg:py-6 border text-center">
                                             Total
@@ -135,13 +131,13 @@ const OrderDetails = () => {
                                                Course name
                                             </td>
                                             <td className="lg:py-6 md:py-4 py-2 text-center border">
-                                               unit name
+                                               Student name
                                             </td>
                                             <td className="lg:py-6 md:py-4 py-2 text-center border">
                                                quantity
                                             </td>
                                             <td className="lg:py-6 md:py-4 py-2 text-center border">
-                                               unit price
+                                                price
                                             </td>
                                             <td className="lg:py-6 md:py-4 py-2 text-center border">
                                                total price
@@ -152,9 +148,7 @@ const OrderDetails = () => {
                             </table>
                         </div>
                     </div>
-                    <div className="lg:w-[30%] w-full mx-auto 0">
-                        <TrackOrder  />
-                    </div>
+                   
                 </div>
             </div>
     );
--- src/Shared/Footer/Copyright.jsx
+++ src/Shared/Footer/Copyright.jsx
@@ -7,7 +7,7 @@ const Copyright = () => {
         <div className="bg-_white border-2 border-bg_lightSlate py-pl_secondary px-2 lg:px-0 mx-2 rounded-sm">
             <div className="max-w-7xl mx-auto flex items-center flex-col lg:flex-row justify-between">
                 <p className="text-center">
-                &copy; {year} All right reserved by Greater Chittagong Association. | Design & Developed by Dhrubo Joyti Das.
+                &copy; {year} All right reserved by Wb Softwares. | Design & Developed by Dhrubo Joyti Das.
                 </p>
                 <p className="">
                     Sitemap | privacy policy
